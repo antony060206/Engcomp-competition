8eead55df0a2164e7cb10c52bf630d5cd1f9b2b4
Antony <anthony060206@icloud.com>
2024-02-20 01:44:45 -0700

    test 2

--- game.py
+++ game.py
@@ -4,5 +4,7 @@ Created on Tue Feb 20 01:15:04 2024
 
 @author: antho
 "welcome to coding"
+- "if you made any comments please do so by ending the comment with
+(intial)" for example:
 """
-
+#This is my comment (A)
